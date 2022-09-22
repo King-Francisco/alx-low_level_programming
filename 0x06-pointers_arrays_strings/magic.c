ea17485f12e3b7126e18afddf4b56095fc7eeab8
Francis Eze <ikechukwufranciseze2019@gmail.com>
2022-09-22 05:43:56 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/magic.c
+++ 0x06-pointers_arrays_strings/magic.c
@@ -14,7 +14,7 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to use modify p
 	 * - only one statement
-	 * - you ate not allowed to code aything else than this line of code
+	 * - you are not allowed to code aything else than this line of code
 	 */
 	;
 	/* ...so that this prints 98\n */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,12 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +17,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+#endif /*MAIN_H*/
